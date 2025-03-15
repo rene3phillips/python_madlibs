6664af0785178fb0aa38dfcd031758f94e5c86e8
rene3phillips <rene3phillips@gmail.com>
2025-03-15 17:02:08 -0400

    Add save story feature

--- mad_libs.py
+++ mad_libs.py
@@ -2,7 +2,7 @@ while True:
 
     # Python Mad Libs Warm-Up Activity
     # Welcome message
-    print("Welcome to Python Mad Libs!")
+    print("\nWelcome to Python Mad Libs!")
     print("Answer the following questions to create your very own silly story.\n")
     # Gather user inputs
     adjective = input("Enter an adjective: ")
@@ -18,10 +18,20 @@ while True:
     print("\nHere is your story:")
     print(story)
 
+    # Save Story to a Text File
+    save_story = input("\nWould you like to save your story to a text file? (yes/no) ").lower().strip()
+    if save_story == "yes":
+        file_name = input("What would you like to name your file? ")
+        with open(f"{file_name}.txt", "w") as file:
+            file.write(story)
+        print(f"Your story has been saved to '{file_name}.txt'.")
+    else:
+        pass
+    
     # Replay Feature
-    play_again = input("Would you like to play again? (yes/no) ").lower().strip()
+    play_again = input("\nWould you like to play again? (yes/no) ").lower().strip()
     if play_again == "yes":
         continue
     else:
-        print("Thank you for playing!")
+        print("Thank you for playing!\n")
         break 
